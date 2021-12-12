22682fae25297174a88f60e2d20745ddf258095e
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2021-12-12 22:58:37 +0800

    enhancement: indent blankline show current context

--- dotfiles/.config/nvim/lua/configs/indent-blankline.lua
+++ dotfiles/.config/nvim/lua/configs/indent-blankline.lua
@@ -2,4 +2,19 @@
 require("indent_blankline").setup {
   buftype_exclude = {'terminal', 'help'},
   filetype_exclude = {"alpha"},
+  show_current_context = true,
 }
+
+vim.cmd[[highlight IndentBlanklineContextChar guifg=#fabd2f gui=nocombine]]
+vim.g.indent_blankline_context_patterns = {
+  '^if',
+  '^while',
+  '^for',
+  '^try',
+  '^catch',
+  '^type',
+  'method',
+  'function',
+  'class',
+}
+
