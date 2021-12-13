a5899bdea9dafc5275d55c8b713324ff381a985e
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2021-12-13 17:04:17 +0800

    enhancement: dashboard

--- dotfiles/.config/nvim/lua/configs/alpha.lua
+++ dotfiles/.config/nvim/lua/configs/alpha.lua
@@ -1,20 +1,121 @@
-
 local alpha = require('alpha')
-local dashboard = require('alpha.themes.dashboard')
-dashboard.section.header.val = {
-[[                                                 ]],
-[[                               __                ]],
-[[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-[[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-[[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-[[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-[[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
-[[                                                 ]],
+
+local header = {
+  type = "text",
+  val = {
+    [[                               __                ]],
+    [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
+    [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+    [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+    [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+    [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+    [[                                                 ]],
+  },
+  opts = {
+    position = "center",
+    hl = "Comment"
+  }
 }
-dashboard.section.buttons.val = {
-dashboard.button( "e", "  New file" , ':ene <BAR> startinsert <CR>'),
-dashboard.button( "f", "  Telescope" , ':lua require("telescope.builtin").find_files(tlscp_opts(""))<cr>'),
-dashboard.button( "q", "  Quit NVIM" , ':qa<CR>'),
+
+local handle = io.popen('fd -d 2 . $HOME"/.local/share/nvim/site/pack/packer" | head -n -2 | wc -l | tr -d "\n" ')
+local plugins = handle:read("*a")
+handle:close()
+
+local thingy = io.popen('echo "$(date +%a) $(date +%d) $(date +%b)" | tr -d "\n"')
+local date = thingy:read("*a")
+thingy:close()
+
+local heading = {
+  type = "text",
+  val = "┌─   Today is " .. date .. " ─┐",
+  opts = {
+    position = "center",
+    hl = "Identifier",
+  }
 }
-dashboard.section.footer.val = require('alpha.fortune')()
-alpha.setup(dashboard.opts)
+
+local plugin_count = {
+  type = "text",
+  val = "└─   " .. plugins .. " plugins in total ─┘",
+  opts = {
+    position = "center",
+    hl = "Identifier",
+  }
+}
+
+local footer = {
+  type = "text",
+  val = require('alpha.fortune')(),
+  opts = {
+    position = "center",
+    width = 50,
+    hl = "Comment",
+    hl_shortcut = "Comment",
+  }
+}
+
+local function button(sc, txt, keybind)
+  local sc_ = sc:gsub("%s", ""):gsub("SPC", "<leader>")
+
+  local opts = {
+    position = "center",
+    text = txt,
+    shortcut = sc,
+    cursor = 5,
+    width = 50,
+    align_shortcut = "right",
+    hl_shortcut = "Number",
+    hl = "Function",
+  }
+  if keybind then
+    opts.keymap = {"n", sc_, keybind, {noremap = true, silent = true}}
+  end
+
+  return {
+    type = "button",
+    val = txt,
+    on_press = function()
+      local key = vim.api.nvim_replace_termcodes(sc_, true, false, true)
+      vim.api.nvim_feedkeys(key, "normal", false)
+    end,
+    opts = opts,
+  }
+end
+
+local buttons = {
+  type = "group",
+  val = {
+    button( "e", "  New file" , ':ene <BAR> startinsert <CR>'),
+    button( "f", "  Telescope" , ':lua require("telescope.builtin").find_files(tlscp_opts(""))<cr>'),
+    button( "q", "  Quit NVIM" , ':qa<CR>'),
+  },
+  opts = {
+    spacing = 1,
+  }
+}
+
+local section = {
+  header = header,
+  buttons = buttons,
+  plugin_count = plugin_count,
+  heading = heading,
+  footer = footer
+}
+
+local opts = {
+  layout = {
+    {type = "padding", val = 4},
+    section.header,
+    {type = "padding", val = 2},
+    section.heading,
+    section.plugin_count,
+    {type = "padding", val = 2},
+    section.buttons,
+    {type = "padding", val = 0},
+    section.footer,
+  },
+  opts = {
+    margin = 5
+  },
+}
+alpha.setup(opts)
