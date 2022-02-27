33ffcf4c3d86f61da2619150c493d8626bede035
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2022-02-27 11:49:32 +0800

    🍁 add nvim

--- setup/package/package.mk
+++ setup/package/package.mk
@@ -9,6 +9,7 @@ ifeq ($(UNAME_S),Darwin)
 	LINK_LOGOLS := https://github.com/Yash-Handa/logo-ls/releases/download/v1.3.7/logo-ls_Darwin_x86_64.tar.gz
 	LINK_NNN := https://github.com/jarun/nnn/releases/download/v4.4/nnn-nerd-static-4.4.x86_64.tar.gz
 	LINK_BTOP := https://github.com/aristocratos/btop/releases/download/v1.2.3/btop-x86_64-macos-monterey.tbz
+	LINK_NVIM := https://github.com/neovim/neovim/releases/download/v0.6.0/nvim-macos.tar.gz
 else
 	LINK_FD := https://github.com/sharkdp/fd/releases/download/v8.3.0/fd-v8.3.0-x86_64-unknown-linux-gnu.tar.gz
 	LINK_RG := https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz
@@ -18,5 +19,6 @@ else
 	LINK_LOGOLS := https://github.com/Yash-Handa/logo-ls/releases/download/v1.3.7/logo-ls_Linux_x86_64.tar.gz
 	LINK_NNN := https://github.com/jarun/nnn/releases/download/v4.4/nnn-nerd-static-4.4.x86_64.tar.gz
 	LINK_BTOP := https://github.com/aristocratos/btop/releases/download/v1.2.3/btop-x86_64-linux-musl.tbz
+	LINK_NVIM := https://github.com/neovim/neovim/releases/download/v0.6.0/nvim-linux64.tar.gz
 endif
 
