0918d37a056340b3331037edd41f100bc722b364
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2022-03-21 18:23:30 +0800

    🍁 update vimwiki config

--- dotfiles/.config/nvim/lua/configs/vimwiki.lua
+++ dotfiles/.config/nvim/lua/configs/vimwiki.lua
@@ -2,9 +2,14 @@
 local my_wiki = {
   ['path'] = '~/Dropbox/wiki/',
   ['syntax'] = 'markdown',
-  ['ext'] = '.md'
+  ['ext'] = '.md',
+  ['auto_toc'] = 1
 }
 
+--[[ toc ]]
+vim.g.vimwiki_toc_header_level = 3
+
+vim.g.vimwiki_markdown_link_ext = 1
 vim.g.vimwiki_auto_chdir = 1
 vim.g.vimwiki_table_mappings = 0
 vim.g.vimwiki_table_auto_fmt = 0
