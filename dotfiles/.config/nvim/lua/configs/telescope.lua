372392a17971c868843136a51d355b42d78a0468
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2022-04-01 12:03:38 +0800

    🍁 telescope default horizontal

--- dotfiles/.config/nvim/lua/configs/telescope.lua
+++ dotfiles/.config/nvim/lua/configs/telescope.lua
@@ -3,9 +3,11 @@ local actions = require("telescope.actions")
 require('telescope').setup{
   defaults = {
     prompt_prefix = "  ",
+    sorting_strategy = "ascending",
     layout_config = {
       horizontal = {
-       preview_width = 0.6,
+        prompt_position = 'top',
+        preview_width = 0.6,
       },
     },
     mappings = {
@@ -26,14 +28,19 @@ require('telescope').setup{
   },
   pickers = {
     find_files = {
-      theme = "ivy",
+      -- theme = "ivy",
+      hidden = true,
+      layout_config = {
+        -- height = 0.6,
+        -- preview_width=0.6
+      }
     },
     diagnostics = {
       theme = "dropdown",
       path_display = "tail",
     },
     lsp_definitions = require("telescope.themes").get_cursor({
-      jump_type="never",
+      -- jump_type="never",
       layout_config = {
         width = 0.8,
         height = 0.5,
@@ -43,9 +50,9 @@ require('telescope').setup{
     lsp_code_actions = require("telescope.themes").get_cursor({
     }),
     buffers = {
+      theme = "dropdown",
       show_all_buffers = true,
       sort_lastused = true,
-      theme = "ivy",
       mappings = {
         n = {
           ["dd"] = "delete_buffer",
