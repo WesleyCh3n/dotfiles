0a4df12b27c6eec6c5f783e1479f0d2eac770a42
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2022-04-01 12:07:28 +0800

    🍁 update btop version

--- setup/package/package.mk
+++ setup/package/package.mk
@@ -7,7 +7,7 @@ ifeq ($(UNAME_S),Darwin)
 	LINK_LAZYGIT := https://github.com/jesseduffield/lazygit/releases/download/v0.31.4/lazygit_0.31.4_Darwin_x86_64.tar.gz
 	LINK_MACCHINA := https://github.com/Macchina-CLI/macchina/releases/download/v6.0.1/macchina-macos-x86_64
 	LINK_NNN := https://github.com/jarun/nnn/releases/download/v4.4/nnn-nerd-static-4.4.x86_64.tar.gz
-	LINK_BTOP := https://github.com/aristocratos/btop/releases/download/v1.2.3/btop-x86_64-macos-monterey.tbz
+	LINK_BTOP := https://github.com/aristocratos/btop/releases/download/v1.2.5/btop-x86_64-macos-monterey.tbz
 	LINK_NVIM := https://github.com/neovim/neovim/releases/download/v0.6.0/nvim-macos.tar.gz
 	LINK_LSD := https://github.com/Peltoche/lsd/releases/download/0.21.0/lsd-0.21.0-x86_64-apple-darwin.tar.gz
 else
@@ -17,7 +17,7 @@ else
 	LINK_LAZYGIT := https://github.com/jesseduffield/lazygit/releases/download/v0.31.4/lazygit_0.31.4_Linux_x86_64.tar.gz
 	LINK_MACCHINA := https://github.com/Macchina-CLI/macchina/releases/download/v6.0.1/macchina-linux-x86_64
 	LINK_NNN := https://github.com/jarun/nnn/releases/download/v4.4/nnn-nerd-static-4.4.x86_64.tar.gz
-	LINK_BTOP := https://github.com/aristocratos/btop/releases/download/v1.2.3/btop-x86_64-linux-musl.tbz
+	LINK_BTOP := https://github.com/aristocratos/btop/releases/download/v1.2.5/btop-x86_64-linux-musl.tbz
 	LINK_NVIM := https://github.com/neovim/neovim/releases/download/v0.6.0/nvim-linux64.tar.gz
 	LINK_LSD := https://github.com/Peltoche/lsd/releases/download/0.21.0/lsd-0.21.0-x86_64-unknown-linux-gnu.tar.gz
 endif
