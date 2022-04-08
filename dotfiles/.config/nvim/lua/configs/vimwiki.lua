4a663cbce102281b5259240aa790ef1b60df0d31
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2022-04-08 15:54:17 +0800

    🍁 update vimwiki

--- dotfiles/.config/nvim/lua/configs/vimwiki.lua
+++ dotfiles/.config/nvim/lua/configs/vimwiki.lua
@@ -8,6 +8,7 @@ local my_wiki = {
     ['python'] = 'python',
     ['c++'] = 'cpp',
     ['rust'] = 'rust',
+    ['go'] = 'go',
   }
 }
 
@@ -15,7 +16,7 @@ local my_wiki = {
 vim.g.vimwiki_toc_header_level = 3
 
 vim.g.vimwiki_markdown_link_ext = 1
-vim.g.vimwiki_auto_chdir = 1
+-- vim.g.vimwiki_auto_chdir = 1
 vim.g.vimwiki_table_mappings = 0
 vim.g.vimwiki_table_auto_fmt = 0
 vim.g.vimwiki_list = { my_wiki }
