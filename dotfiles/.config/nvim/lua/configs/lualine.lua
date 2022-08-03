b52c5edbe9128000ef7ffb0668661c014a528db1
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2022-08-03 17:02:34 +0800

    🎨 Hide in alpha

--- dotfiles/.config/nvim/lua/configs/lualine.lua
+++ dotfiles/.config/nvim/lua/configs/lualine.lua
@@ -36,7 +36,7 @@ lualine.setup {
     theme = gruvbox_theme,
     section_separators = { left = '', right = ''},
     component_separators = { left = '|', right = '|'},
-    disabled_filetypes = {"NvimTree", "lspsagaoutline"},
+    disabled_filetypes = {"NvimTree", "lspsagaoutline", "alpha"},
     always_divide_middle = true,
   },
   sections = {
