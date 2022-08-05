4fee7f888688c177c84a6c0284dda4308705c31c
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2022-08-05 15:44:25 +0800

    🔥 Remove unused code

--- dotfiles/.config/nvim/lua/configs/commenter.lua
+++
@@ -1,9 +0,0 @@
-
-vim.g.NERDSpaceDelims = 1
-vim.g.NERDCompactSexyComs = 1
-vim.g.NERDTrimTrailingWhitespace = 1
-vim.g.NERDToggleCheckAllLines = 1
-vim.g.NERDCustomDelimiters = {
-    python = { left = '#', right = '' },
-    dosbatch = { left = '::' }
-}
--- dotfiles/.config/nvim/lua/configs/nvim-repl.lua
+++
@@ -1,7 +0,0 @@
-
-vim.g.repl_filetype_commands = {
-    javascript = 'node',
-    python = 'python3',
-}
-vim.g.repl_split = 'bottom'
-vim.g.repl_height = 10
