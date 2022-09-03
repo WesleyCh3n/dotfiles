bb64ee8e11cf72347960013df36b331fa72f3dc1
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2022-09-03 14:17:55 +0800

    🎨 use shallow clone

--- scripts/aarch64/nnn.sh
+++ scripts/aarch64/nnn.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
 
 sudo apt-get install pkg-config libncursesw5-dev libreadline-dev
-git clone https://github.com/jarun/nnn && cd nnn
+git clone --depth=1 https://github.com/jarun/nnn && cd nnn
 make strip install PREFIX=~/.local
--- scripts/aarch64/nvim.sh
+++ scripts/aarch64/nvim.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
 
 sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
-git clone https://github.com/neovim/neovim
+git clone --depth=1 https://github.com/neovim/neovim
 cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=~/.local
