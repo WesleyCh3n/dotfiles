d7338192aeb59e95ef312e889c715c935893ef35
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2023-05-05 18:29:30 +0800

    update: filter out wrong output

--- dotfiles/.config/nvim/lua/modules/ui/plugins.lua
+++ dotfiles/.config/nvim/lua/modules/ui/plugins.lua
@@ -38,5 +38,14 @@ plugin({
   'rcarriga/nvim-notify',
   config = function()
     vim.notify = require("notify")
+    local banned_messages = { "No information available" }
+    vim.notify = function(msg, ...)
+      for _, banned in ipairs(banned_messages) do
+        if msg == banned then
+          return
+        end
+      end
+      return require("notify")(msg, ...)
+    end
   end
 })
