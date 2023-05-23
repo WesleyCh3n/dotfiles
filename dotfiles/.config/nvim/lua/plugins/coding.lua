8b90e40e271727ccf1a6d3db00239cc1e3bb58c1
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2023-05-23 17:25:03 +0800

    fix(nvim): remove unpack deprecated

--- dotfiles/.config/nvim/lua/plugins/coding.lua
+++ dotfiles/.config/nvim/lua/plugins/coding.lua
@@ -68,7 +68,8 @@ return {
         if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
           return false
         end
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        local line = vim.api.nvim_win_get_cursor(0)[1]
+        local col = vim.api.nvim_win_get_cursor(0)[2]
         return col ~= 0 and
             vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
       end
