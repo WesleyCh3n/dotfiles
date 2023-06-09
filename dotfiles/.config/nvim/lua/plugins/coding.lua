954a0450417e6f4a85931b03e81631b2d18ec6a7
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2023-06-09 14:37:02 +0800

    fix(nvim): enable snippet

--- dotfiles/.config/nvim/lua/plugins/coding.lua
+++ dotfiles/.config/nvim/lua/plugins/coding.lua
@@ -1,9 +1,9 @@
 return {
   {
     'L3MON4D3/LuaSnip',
-    version = "1.*",
+    -- version = "1.*",
     config = function()
-      -- require("luasnip.loaders.from_snipmate").lazy_load()
+      require("luasnip.loaders.from_snipmate").lazy_load()
     end,
     dependencies = {
       { 'honza/vim-snippets' },
