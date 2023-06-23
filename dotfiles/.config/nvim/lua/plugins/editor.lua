b2aa2846860969acb703f862e32314d19580f0c4
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2023-06-23 16:55:42 +0800

    add(nvim): harpoon

--- dotfiles/.config/nvim/lua/plugins/editor.lua
+++ dotfiles/.config/nvim/lua/plugins/editor.lua
@@ -377,4 +377,18 @@ return {
       { '<space>z', function() require("zen-mode").toggle({}) end, desc = "ZenMode", }
     }
   },
+
+  -- harpoon
+  {
+    "ThePrimeagen/harpoon",
+    opts = {},
+    keys = {
+      { '<leader>a', function() require("harpoon.mark").add_file() end,        desc = "Add to harpoon", },
+      { '<leader>e', function() require("harpoon.ui").toggle_quick_menu() end, desc = "Open harpoon ui", },
+      { '<leader>1', function() require("harpoon.ui").nav_file(1) end },
+      { '<leader>2', function() require("harpoon.ui").nav_file(2) end },
+      { '<leader>3', function() require("harpoon.ui").nav_file(3) end },
+      { '<leader>4', function() require("harpoon.ui").nav_file(4) end },
+    }
+  },
 }
