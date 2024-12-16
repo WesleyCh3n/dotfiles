30c7de087601036e7bd5ddf498b2e9363cb87902
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2024-12-16 11:43:49 +0800

    feat: win config

--- dotfiles/.config/nvim/lua/plugins/toggleterm.lua
+++ dotfiles/.config/nvim/lua/plugins/toggleterm.lua
@@ -23,7 +23,7 @@ return {
         size = 10,
         direction = 'horizontal',
         shell = (vim.loop.os_uname().sysname ~= "Windows_NT")
-            and vim.o.shell or "nu.exe",
+            and vim.o.shell or "cmd.exe",
         float_opts = {
           border = 'curved'
         },
