351bc3771e2137a9ea13d987b3a960fadb7a6b79
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2025-05-05 18:05:41 +0800

    feat(lsp): add message for keymap

--- dotfiles/.config/nvim/lua/plugins/lsp/keymaps.lua
+++ dotfiles/.config/nvim/lua/plugins/lsp/keymaps.lua
@@ -1,37 +1,41 @@
 local M = {}
 
 M.on_attach = function(client, bufnr)
-  local opts = { buffer = bufnr, noremap = true, silent = true }
+  -- local opts = { buffer = bufnr, noremap = true, silent = true }
   local telescope = require("telescope.builtin")
   -- go to definitions
   vim.keymap.set('n', 'gf', function()
     telescope.lsp_definitions {}
-  end, opts)
-  -- vim.keymap.set('n', 'gi', '<cmd>Telescope lsp_implementations<CR>', opts)
-  -- vim.keymap.set('n', '<space>rr', '<cmd>lua vim.lsp.buf.format(opts)<CR>', opts)
+  end, { buffer = bufnr, noremap = true, silent = true, desc = "definitions" })
+  -- vim.keymap.set('n', 'gi', '<cmd>Telescope lsp_implementations<CR>', { buffer = bufnr, noremap = true, silent = true })
 
   -- reference
   vim.keymap.set('n', 'gr', function()
     telescope.lsp_references {}
-  end, opts)
+  end, { buffer = bufnr, noremap = true, silent = true, desc = "reference" })
 
   -- definitions
   vim.keymap.set('n', 'K', function()
     vim.lsp.buf.hover()
-  end, opts)
-  -- vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  end, { buffer = bufnr, noremap = true, silent = true, desc = "hover" })
+  -- vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<CR>', { buffer = bufnr, noremap = true, silent = true })
 
   -- diagnostic
   vim.keymap.set("n", 'gl', function()
     telescope.diagnostics { severity_bound = 0, bufnr = nil }
-  end, opts)
-  vim.keymap.set('n', 'gs', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
-  vim.keymap.set('n', 'gn', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-  vim.keymap.set('n', 'gp', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+  end, { buffer = bufnr, noremap = true, silent = true, desc = "diagnostics" })
+  vim.keymap.set('n', 'gs', '<cmd>lua vim.diagnostic.open_float()<cr>',
+    { buffer = bufnr, noremap = true, silent = true, desc = "diagnostic expand" })
+  vim.keymap.set('n', 'gn', '<cmd>lua vim.diagnostic.goto_next()<CR>',
+    { buffer = bufnr, noremap = true, silent = true, desc = "diagnostic next" })
+  vim.keymap.set('n', 'gp', '<cmd>lua vim.diagnostic.goto_prev()<CR>',
+    { buffer = bufnr, noremap = true, silent = true, desc = "diagnostic prev" })
 
   -- refactor
-  vim.keymap.set('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-  vim.keymap.set('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  vim.keymap.set('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<cr>',
+    { buffer = bufnr, noremap = true, silent = true, desc = "code action" })
+  vim.keymap.set('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>',
+    { buffer = bufnr, noremap = true, silent = true, desc = "rename" })
 
   if client.server_capabilities.documentSymbolProvider then
     require("nvim-navic").attach(client, bufnr)
