4ce290ab8c324e992b4ccb6b1939a5d940b3e39d
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2025-05-17 18:02:01 +0800

    fix(nvim): not enable by defaule

--- dotfiles/.config/nvim/lua/plugins/typst-preview.lua
+++ dotfiles/.config/nvim/lua/plugins/typst-preview.lua
@@ -1,8 +1,8 @@
 return {
   {
-    'chomosuke/typst-preview.nvim',
-    lazy = false, -- or ft = 'typst'
-    version = '1.*',
-    opts = {},    -- lazy.nvim will implicitly calls `setup {}`
+    -- 'chomosuke/typst-preview.nvim',
+    -- lazy = false, -- or ft = 'typst'
+    -- version = '1.*',
+    -- opts = {},    -- lazy.nvim will implicitly calls `setup {}`
   }
 }
