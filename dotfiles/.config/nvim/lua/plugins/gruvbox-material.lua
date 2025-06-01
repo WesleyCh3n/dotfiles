afc1e8726431bc9c2c27a3110ecc176240a1b376
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2025-06-02 00:49:30 +0800

    refactor(nvim): remove highlight group

--- dotfiles/.config/nvim/lua/plugins/gruvbox-material.lua
+++ dotfiles/.config/nvim/lua/plugins/gruvbox-material.lua
@@ -43,45 +43,45 @@ return {
         WhichKeyBorder             = { fg = c.fg0[1], bg = "NONE" },
       }
       local cmp_groups = {
-        PmenuSel                 = { bg = "#32302f", fg = "NONE", blend = 0 },
-        Pmenu                    = { fg = '#ddc7a1', bg = '#45403d', blend = 0 },
+        PmenuSel = { bg = "#32302f", fg = "NONE", blend = 0 },
+        Pmenu    = { fg = '#ddc7a1', bg = '#45403d', blend = 0 },
 
-        CmpItemAbbrDeprecated    = { fg = "#7E8294", bg = "NONE", strikethrough = true, italic = true },
-        CmpItemAbbrMatch         = { fg = "#82AAFF", bg = "NONE", bold = true },
-        CmpItemAbbrMatchFuzzy    = { fg = "#82AAFF", bg = "NONE", bold = true },
-        CmpItemMenu              = { fg = "#C792EA", bg = "NONE" },
-
-        CmpItemKindField         = { fg = "#EED8DA", bg = "#B5585F" },
-        CmpItemKindProperty      = { fg = "#EED8DA", bg = "#B5585F" },
-        CmpItemKindEvent         = { fg = "#EED8DA", bg = "#B5585F" },
-        CmpItemKindText          = { fg = "#C3E88D", bg = "#9FBD73" },
-        CmpItemKindEnum          = { fg = "#C3E88D", bg = "#9FBD73" },
-        CmpItemKindKeyword       = { fg = "#C3E88D", bg = "#9FBD73" },
-
-        CmpItemKindConstant      = { fg = "#FFE082", bg = "#D4BB6C" },
-        CmpItemKindConstructor   = { fg = "#FFE082", bg = "#D4BB6C" },
-        CmpItemKindReference     = { fg = "#FFE082", bg = "#D4BB6C" },
-
-        CmpItemKindFunction      = { fg = "#EADFF0", bg = "#A377BF" },
-        CmpItemKindStruct        = { fg = "#EADFF0", bg = "#A377BF" },
-        CmpItemKindClass         = { fg = "#EADFF0", bg = "#A377BF" },
-        CmpItemKindModule        = { fg = "#EADFF0", bg = "#A377BF" },
-        CmpItemKindOperator      = { fg = "#EADFF0", bg = "#A377BF" },
-
-        CmpItemKindVariable      = { fg = "#C5CDD9", bg = "#7E8294" },
-        CmpItemKindFile          = { fg = "#C5CDD9", bg = "#7E8294" },
-
-        CmpItemKindUnit          = { fg = "#F5EBD9", bg = "#D4A959" },
-        CmpItemKindSnippet       = { fg = "#F5EBD9", bg = "#D4A959" },
-        CmpItemKindFolder        = { fg = "#F5EBD9", bg = "#D4A959" },
-
-        CmpItemKindMethod        = { fg = "#DDE5F5", bg = "#6C8ED4" },
-        CmpItemKindValue         = { fg = "#DDE5F5", bg = "#6C8ED4" },
-        CmpItemKindEnumMember    = { fg = "#DDE5F5", bg = "#6C8ED4" },
-
-        CmpItemKindInterface     = { fg = "#D8EEEB", bg = "#58B5A8" },
-        CmpItemKindColor         = { fg = "#D8EEEB", bg = "#58B5A8" },
-        CmpItemKindTypeParameter = { fg = "#D8EEEB", bg = "#58B5A8" },
+        -- BlinkCmpItemAbbrDeprecated    = { fg = "#7E8294", bg = "NONE", strikethrough = true, italic = true },
+        -- BlinkCmpItemAbbrMatch         = { fg = "#82AAFF", bg = "NONE", bold = true },
+        -- BlinkCmpItemAbbrMatchFuzzy    = { fg = "#82AAFF", bg = "NONE", bold = true },
+        -- BlinkCmpItemMenu              = { fg = "#C792EA", bg = "NONE" },
+        --
+        -- BlinkCmpItemKindField         = { fg = "#EED8DA", bg = "#B5585F" },
+        -- BlinkCmpItemKindProperty      = { fg = "#EED8DA", bg = "#B5585F" },
+        -- BlinkCmpItemKindEvent         = { fg = "#EED8DA", bg = "#B5585F" },
+        -- BlinkCmpItemKindText          = { fg = "#C3E88D", bg = "#9FBD73" },
+        -- BlinkCmpItemKindEnum          = { fg = "#C3E88D", bg = "#9FBD73" },
+        -- BlinkCmpItemKindKeyword       = { fg = "#C3E88D", bg = "#9FBD73" },
+        --
+        -- BlinkCmpItemKindConstant      = { fg = "#FFE082", bg = "#D4BB6C" },
+        -- BlinkCmpItemKindConstructor   = { fg = "#FFE082", bg = "#D4BB6C" },
+        -- BlinkCmpItemKindReference     = { fg = "#FFE082", bg = "#D4BB6C" },
+        --
+        -- BlinkCmpItemKindFunction      = { fg = "#EADFF0", bg = "#A377BF" },
+        -- BlinkCmpItemKindStruct        = { fg = "#EADFF0", bg = "#A377BF" },
+        -- BlinkCmpItemKindClass         = { fg = "#EADFF0", bg = "#A377BF" },
+        -- BlinkCmpItemKindModule        = { fg = "#EADFF0", bg = "#A377BF" },
+        -- BlinkCmpItemKindOperator      = { fg = "#EADFF0", bg = "#A377BF" },
+        --
+        -- BlinkCmpItemKindVariable      = { fg = "#C5CDD9", bg = "#7E8294" },
+        -- BlinkCmpItemKindFile          = { fg = "#C5CDD9", bg = "#7E8294" },
+        --
+        -- BlinkCmpItemKindUnit          = { fg = "#F5EBD9", bg = "#D4A959" },
+        -- BlinkCmpItemKindSnippet       = { fg = "#F5EBD9", bg = "#D4A959" },
+        -- BlinkCmpItemKindFolder        = { fg = "#F5EBD9", bg = "#D4A959" },
+        --
+        -- BlinkCmpItemKindMethod        = { fg = "#DDE5F5", bg = "#6C8ED4" },
+        -- BlinkCmpItemKindValue         = { fg = "#DDE5F5", bg = "#6C8ED4" },
+        -- BlinkCmpItemKindEnumMember    = { fg = "#DDE5F5", bg = "#6C8ED4" },
+        --
+        -- BlinkCmpItemKindInterface     = { fg = "#D8EEEB", bg = "#58B5A8" },
+        -- BlinkCmpItemKindColor         = { fg = "#D8EEEB", bg = "#58B5A8" },
+        -- BlinkCmpItemKindTypeParameter = { fg = "#D8EEEB", bg = "#58B5A8" },
       }
       for group, spec in pairs(float_group) do
         vim.api.nvim_set_hl(0, group, spec)
