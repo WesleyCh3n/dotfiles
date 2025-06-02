50199373996972af48b56c2a6bf08245f45591b8
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2025-06-02 12:22:19 +0800

    fix(nvim): disable in blink & popup

--- dotfiles/.config/nvim/lua/plugins/nvim-scrollbar.lua
+++ dotfiles/.config/nvim/lua/plugins/nvim-scrollbar.lua
@@ -10,9 +10,18 @@ return {
       handle = {
         color = '#665c54',
       },
+
+      excluded_buftypes = {
+        "nofile",
+        "popup",
+      },
       excluded_filetypes = {
+        "popup",
         "cmp_menu",
-        "cmp_docs"
+        "cmp_docs",
+        "blink-cmp-menu",
+        "blink-cmp-signature",
+        "blink-cmp-documentation",
       },
     }
   },
