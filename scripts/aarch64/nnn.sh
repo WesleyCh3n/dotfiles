6b696bb7ccb8a10c42f86dacb40b8371e4c9802a
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2025-06-23 10:53:14 +0800

    chore: remove unused script

--- scripts/aarch64/nnn.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env zsh
-
-sudo apt-get install pkg-config libncursesw5-dev libreadline-dev
-git clone --depth=1 https://github.com/jarun/nnn && cd nnn
-make strip install PREFIX=~/.local
