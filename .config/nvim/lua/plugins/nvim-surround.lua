3f2839780ece1ad1afcde681ed8d87c31d5c655e
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2026-03-24 15:38:33 -0400

    fix(nvim): surround & flash keymap

--- .config/nvim/lua/plugins/nvim-surround.lua
+++ .config/nvim/lua/plugins/nvim-surround.lua
@@ -2,11 +2,13 @@ return {
   {
     "kylechui/nvim-surround",
     event = "VeryLazy",
-    opts = {
-      keymaps = {
-        change = "ts",
-        change_line = "tS"
-      },
-    }
+    config = function(_, _)
+      vim.keymap.set("n", "gs", "<Plug>(nvim-surround-change)", {
+        desc = "Change a surrounding pair",
+      })
+      vim.keymap.set("n", "gS", "<Plug>(nvim-surround-change-line)", {
+        desc = "Change a surrounding pair, putting replacements on new lines",
+      })
+    end,
   },
 }
