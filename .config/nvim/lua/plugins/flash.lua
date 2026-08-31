35ed2bf1714fa11c0c36de95d1a0a3dc08639c18
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2026-08-31 10:39:00 -0400

    fix(nvim): flash key binding

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -4,8 +4,8 @@ return {
     event = "VeryLazy",
     opts = {},
     keys = {
-      { "<c-space>", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
-      { "S",         mode = { "n" },           function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
+      { "S", mode = { "n" },           function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     },
   },
 }
